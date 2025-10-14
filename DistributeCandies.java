b0eb2d2bf63f2e185fe6ba60fe1464010ac73315
TheIshan07 <paitwarvg@rknec.edu>
2025-10-14 14:14:19 +0530

    575. Distribute Candies

--- DistributeCandies.java
+++ DistributeCandies.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 class Solution {
     public int distributeCandies(int[] candyType) {
-        HashSet<Integer> distinctCandies = new HashSet<>();
+        HashSet<Integer> diffCandies = new HashSet<>();
         
         for (int candy : candyType) {
-            distinctCandies.add(candy);
+            diffCandies.add(candy);
         }
         
         int maxDistinctCandies = candyType.length / 2;
         
-        return Math.min(distinctCandies.size(), maxDistinctCandies);
+        return Math.min(diffCandies.size(), maxDistinctCandies);
     }
 }

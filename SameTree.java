f3702cd006380098262806d0c74c32594fc73495
TheIshan07 <paitwarvg@rknec.edu>
2025-12-22 11:27:43 +0530

    100.Same Tree

--- SameTree.java
+++ SameTree.java
@@ -14,15 +14,15 @@
  *         this.right = right;
  *     }
  * }
- */
-class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null && q==null){
-            return true;
-        }
-        if(p == null || q == null){
-            return false;
-        }
-        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-    }
-}
+//  */
+// class Solution {
+//     public boolean isSameTree(TreeNode p, TreeNode q) {
+//         if(p==null && q==null){
+//             return true;
+//         }
+//         if(p == null || q == null){
+//             return false;
+//         }
+//         return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+//     }
+// }

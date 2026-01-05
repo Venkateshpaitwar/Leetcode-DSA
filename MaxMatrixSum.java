8497eca142b9d843c3aabb0336a140163f3fdc27
TheIshan07 <paitwarvg@rknec.edu>
2026-01-05 15:23:22 +0530

    1975. Maximum Matrix Sum

--- MaxMatrixSum.java
+++ MaxMatrixSum.java
@@ -2,26 +2,25 @@
 
 class Solution {
     public long maxMatrixSum(int[][] matrix) {
-        int n = matrix.length;
-        long totalSum = 0;
-        int negativeCount = 0;
-        int minAbsValue = Integer.MAX_VALUE;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+         int n = matrix.length;
+         long totalSum = 0;
+         int minAbsValue = Integer.MAX_VALUE;
+         int count = 0;
+         for(int i = 0; i<n; i++){
+            for(int j = 0; j<n; j++){
                 int value = matrix[i][j];
                 totalSum += Math.abs(value);
-                if (value < 0) {
-                    negativeCount++;
+                if(value < 0){
+                    count++;
                 }
                 minAbsValue = Math.min(minAbsValue, Math.abs(value));
             }
-        }
-
-        if (negativeCount % 2 == 0) {
+         }
+         if(count % 2 == 0){
             return totalSum;
-        } else {
+         }
+         else{
             return totalSum - 2 * minAbsValue;
-        }
+         }
     }
 }

1340a3f3496d9ee540af417d95bf1fb894b0099a
Venkateshpaitwar <venkateshpaitwar@gmail.com>
2026-01-09 16:49:54 +0530

    234. Palindrome Linked List.

---
+++ PalidromeLL.java
@@ -0,0 +1,42 @@
+// 234. Palindrome Linked List
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+
+        // Step 1: Find middle
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Step 2: Reverse second half
+        ListNode secondHalf = reverseLL(slow);
+        ListNode firstHalf = head;
+
+        // Step 3: Compare
+        while (secondHalf != null) {
+            if (firstHalf.val != secondHalf.val) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
+    public ListNode reverseLL(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}

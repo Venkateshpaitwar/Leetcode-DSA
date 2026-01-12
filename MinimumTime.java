8219eb9e120f65fedfb268207aee784ec2aee154
Venkateshpaitwar <venkateshpaitwar@gmail.com>
2026-01-12 17:03:52 +0530

    1266. Minimum Time Visiting All Points

---
+++ MinimumTime.java
@@ -0,0 +1,12 @@
+//1266. Minimum Time Visiting All Points
+class Solution {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int minTime = 0;
+        for(int i = 1; i < points.length; i++){
+            int x = Math.abs(points[i][0] - points[i-1][0]);
+            int y = Math.abs(points[i][1] - points[i-1][1]);
+            minTime += Math.max(x,y);
+        }
+        return minTime;
+    }
+}

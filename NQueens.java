1f9fa6ef10d90dbc82457e50b83ba1e4225948b7
Venkateshpaitwar <venkateshpaitwar@gmail.com>
2026-01-19 22:25:04 +0530

    51.N-Queens

--- NQueens.java
+++ NQueens.java
@@ -1,51 +1,66 @@
-// 51.N-Queens
-import java.util.*;
-class Solution {
-    List<List<String>> result;
+//51. N-Queens
 
+import java.util.*;
+
+class Solution {
     public List<List<String>> solveNQueens(int n) {
-        result = new ArrayList<>();
+        List<List<String>> allBoards = new ArrayList<>();
         char[][] board = new char[n][n];
 
         for (int i = 0; i < n; i++) {
             Arrays.fill(board[i], '.');
         }
 
-        dfs(board, 0, new ArrayList<>());
-        return result;
+        helper(board, allBoards, 0);
+        return allBoards;
     }
 
-    private void dfs(char[][] board, int r, List<int[]> queens) {
-        if (r == board.length) {
-            List<String> rows = new ArrayList<>();
-            for (char[] row : board) {
-                rows.add(new String(row));
-            }
-            result.add(rows);
+    private void helper(char[][] board, List<List<String>> allBoards, int col) {
+        if (col == board.length) {
+            saveBoard(board, allBoards);
             return;
         }
 
-        for (int c = 0; c < board.length; c++) {
-            if (canAddQueen(r, c, queens)) {
-                board[r][c] = 'Q';
-                queens.add(new int[]{r, c});
-
-                dfs(board, r + 1, queens);
-
-                board[r][c] = '.';
-                queens.remove(queens.size() - 1);
+        for (int row = 0; row < board.length; row++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = 'Q';
+                helper(board, allBoards, col + 1);
+                board[row][col] = '.';
             }
         }
     }
 
-    private boolean canAddQueen(int r, int c, List<int[]> queens) {
-        for (int[] q : queens) {
-            int dx = Math.abs(r - q[0]);
-            int dy = Math.abs(c - q[1]);
-            if (dy == 0 || dx == dy) {
+    private boolean isSafe(char[][] board, int row, int col) {
+        int n = board.length;
+
+        for (int j = 0; j < col; j++) {
+            if (board[row][j] == 'Q') {
                 return false;
             }
         }
+
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        for (int i = row, j = col; i < n && j >= 0; i++, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
         return true;
+    }
+
+    private void saveBoard(char[][] board, List<List<String>> allBoards) {
+        List<String> newBoard = new ArrayList<>();
+
+        for (int i = 0; i < board.length; i++) {
+            newBoard.add(new String(board[i]));
+        }
+
+        allBoards.add(newBoard);
     }
 }

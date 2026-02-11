f19a87aed9799e6f27a7363a9e357dbdb1d8f20e
Venkateshpaitwar <venkateshpaitwar@gmail.com>
2026-02-11 17:22:09 +0530

    59. Spiral Matrix II

---
+++ SpiralMatrix.java
@@ -0,0 +1,32 @@
+//59. Spiral Matrix II
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        int num = 1;
+        int top = 0; int bottom = n-1;
+        int left = 0; int right = n-1;
+         while (top <= bottom && left <= right) {
+            for (int col = left; col <= right; col++) {
+                matrix[top][col] = num++;
+            }
+            top++;
+            for (int row = top; row <= bottom; row++) {
+                matrix[row][right] = num++;
+            }
+            right--;
+            if (top <= bottom) {
+                for (int col = right; col >= left; col--) {
+                    matrix[bottom][col] = num++;
+                }
+                bottom--;
+            }
+            if (left <= right) {
+                for (int row = bottom; row >= top; row--) {
+                    matrix[row][left] = num++;
+                }
+                left++;
+            }
+        }
+        return matrix;
+    }
+}

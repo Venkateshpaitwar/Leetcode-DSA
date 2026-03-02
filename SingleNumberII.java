729c92d62fb846eea2a66f7481a6ea2eb22dc70e
Venkateshpaitwar <venkateshpaitwar@gmail.com>
2026-03-02 18:38:38 +0530

    3 different approaches for 137. Single Number II

--- SingleNumberII.java
+++ SingleNumberII.java
@@ -11,3 +11,29 @@ class Solution {
         return nums[nums.length - 1];
     }
 }
+// class Solution {
+//   public int singleNumber(int[] nums) {
+//     int ans = 0;
+
+//     for (int i = 0; i < 32; ++i) {
+//       int sum = 0;
+//       for (final int num : nums)
+//         sum += num >> i & 1;
+//       sum %= 3;
+//       ans |= sum << i;
+//     }
+
+//     return ans;
+//   }
+// }
+
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         int ones = 0, twos = 0;
+//         for(int num : nums){
+//             ones = (ones ^ num) & ~twos;
+//             twos = (twos ^ num) & ~ones;
+//         }
+//         return ones;
+//     }
+// }

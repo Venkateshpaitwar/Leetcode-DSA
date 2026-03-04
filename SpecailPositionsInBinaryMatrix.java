d60074dc8c207596f9dcb29789348806cea4c0d5
Venkateshpaitwar <venkateshpaitwar@gmail.com>
2026-03-04 12:58:28 +0530

    1582. Special Positions in a Binary Matrix

---
+++ SpecailPositionsInBinaryMatrix.java
@@ -0,0 +1,27 @@
+// 1582. Special Positions in a Binary Matrix
+
+class Solution {
+    public int numSpecial(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[] rowCount = new int[m];
+        int[] colCount = new int[n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(mat[i][j] == 1){
+                    rowCount[i]++;
+                    colCount[j]++;
+                }
+            }
+        }
+        int spl = 0;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(mat[i][j] == 1 && rowCount[i] == 1 && colCount[j] == 1){
+                    spl++;
+                }
+            }
+        }
+        return spl;
+    }
+}

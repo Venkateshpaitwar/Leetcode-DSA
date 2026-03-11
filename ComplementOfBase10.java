d3a1f75aee5f6b6f8e8c5195483f5068823ac79b
Venkateshpaitwar <venkateshpaitwar@gmail.com>
2026-03-11 09:31:20 +0530

    1009. Complement of Base 10 Integer:

---
+++ ComplementOfBase10.java
@@ -0,0 +1,14 @@
+//c1009. Complement of Base 10 Integer
+
+class Solution {
+    public int bitwiseComplement(int n) {
+        if(n == 0){
+            return 1;
+        }
+        int mask = 1;
+        while(mask <= n){
+            mask <<= 1;
+        }
+        return (mask - 1) ^ n;
+    }
+}

73cad19c0416187e06ee2c4cd2a9dba5e0238309
Venkateshpaitwar <venkateshpaitwar@gmail.com>
2026-03-14 12:51:17 +0530

    52. N-Queens II

---
+++ NQueensII.java
@@ -0,0 +1,65 @@
+// 52. N-Queens II
+
+import java.util.*;
+class Solution {
+    public int totalNQueens(int n) {
+        List<List<String>> allBoards = new ArrayList<>();
+        char[][] board = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(board[i], '.');
+        }
+
+        return helper(board, allBoards, 0);
+    }
+
+    private int helper(char[][] board, List<List<String>> allBoards, int col) {
+        if (col == board.length) {
+            saveBoard(board, allBoards);
+            return allBoards.size();
+        }
+
+        for (int row = 0; row < board.length; row++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = 'Q';
+                helper(board, allBoards, col + 1);
+                board[row][col] = '.';
+            }
+        }
+        return allBoards.size();
+    }
+
+    private boolean isSafe(char[][] board, int row, int col) {
+        int n = board.length;
+
+        for (int j = 0; j < col; j++) {
+            if (board[row][j] == 'Q') {
+                return false;
+            }
+        }
+
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        for (int i = row, j = col; i < n && j >= 0; i++, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void saveBoard(char[][] board, List<List<String>> allBoards) {
+        List<String> newBoard = new ArrayList<>();
+
+        for (int i = 0; i < board.length; i++) {
+            newBoard.add(new String(board[i]));
+        }
+
+        allBoards.add(newBoard);
+    }
+}

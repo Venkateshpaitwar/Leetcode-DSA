7e028451540e3311b1f18b887f47505cd50ae4dd
Venkateshpaitwar <venkateshpaitwar@gmail.com>
2026-03-17 20:00:46 +0530

    75. Sort Colors

---
+++ SortColors.java
@@ -0,0 +1,21 @@
+// 75. Sort Colors
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int cnt0 = 0;
+        int cnt1 = 0;
+        for (int num : nums) {
+            if (num == 0) cnt0++;
+            else if (num == 1) cnt1++;
+        }
+        for (int i = 0; i < cnt0; i++) {
+            nums[i] = 0;
+        }
+        for (int i = cnt0; i <cnt0 + cnt1; i++) {
+            nums[i] = 1;
+        }
+        for (int i = cnt0 + cnt1; i < nums.length; i++) {
+            nums[i] = 2;
+        }
+    }
+}

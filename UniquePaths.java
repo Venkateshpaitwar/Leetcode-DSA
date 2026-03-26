e8e67f642212d5ab135b081907c95883ddd8b57b
Venkateshpaitwar <venkateshpaitwar@gmail.com>
2026-03-26 23:25:10 +0530

    62. Unique Paths

---
+++ UniquePaths.java
@@ -0,0 +1,29 @@
+// 62. Unique Paths
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                dp[i][j] = -1;
+            }
+        }
+        return returnPaths(m-1, n-1, dp);
+    }
+
+    private int returnPaths(int i, int j, int[][] dp){
+        if(i == 0 && j == 0){
+            return 1;
+        } 
+        if(i < 0 || j < 0){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        } 
+        int up = returnPaths(i-1, j, dp);
+        int left = returnPaths(i, j-1, dp);
+        return dp[i][j] = up + left;
+    }
+}

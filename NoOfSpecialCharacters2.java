91ffedf7add4ef62899e0b3048fa9bcd2695224c
Venkateshpaitwar <venkateshpaitwar@gmail.com>
2026-05-27 12:15:35 +0530

    3121. Count the Number of Special Characters II:

---
+++ NoOfSpecialCharacters2.java
@@ -0,0 +1,15 @@
+// 3121. Count the Number of Special Characters II
+
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        int count = 0;
+        for(char c = 'a'; c <= 'z'; c++){
+            int lower = word.lastIndexOf(c);
+            int upper = word.indexOf(Character.toUpperCase(c));
+            if(lower != -1 && upper != -1 && lower < upper){
+                count++;
+            }
+        }
+        return count;
+    }
+}

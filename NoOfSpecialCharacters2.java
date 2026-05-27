0b4c9726a8ac2baa2fe10bd78618d3b3ddc52295
Venkateshpaitwar <venkateshpaitwar@gmail.com>
2026-05-27 12:16:12 +0530

    3121. Count the Number of Special Characters II

--- NoOfSpecialCharacters2.java
+++ NoOfSpecialCharacters2.java
@@ -4,7 +4,7 @@ class Solution {
     public int numberOfSpecialChars(String word) {
         int count = 0;
         for(char c = 'a'; c <= 'z'; c++){
-            int lower = word.lastIndexOf(c);
+            int lower = word.lastIndexOf(c);    //stores the last occurence
             int upper = word.indexOf(Character.toUpperCase(c));
             if(lower != -1 && upper != -1 && lower < upper){
                 count++;

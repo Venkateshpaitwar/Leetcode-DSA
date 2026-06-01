24fe351d7d9c05a99cf08949302666ea57ad611d
Venkateshpaitwar <venkateshpaitwar@gmail.com>
2026-06-01 13:35:25 +0530

    2144. Minimum Cost of Buying Candies With Discount

---
+++ BuyingCandiesWithDiscount.java
@@ -0,0 +1,15 @@
+// 2144. Minimum Cost of Buying Candies With Discount
+
+class Solution {
+    public int minimumCost(int[] cost) {
+        Arrays.sort(cost);
+        int minCost = 0;
+        for(int i = cost.length - 1; i >= 0; i-=3){
+            minCost += cost[i];
+            if(i - 1 >= 0){
+                minCost += cost[i-1];
+            }
+        }
+        return minCost;
+    }
+}

f8ff19ed13b78eed44d6fd1ea0536f362ca118b8
Venkateshpaitwar <venkateshpaitwar@gmail.com>
2026-06-09 11:19:37 +0530

    3689. Maximum Total Subarray Value I

---
+++ MaxTotalSubArrayValueI.java
@@ -0,0 +1,14 @@
+// 3689. Maximum Total Subarray Value I
+
+class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE; 
+        for(int i = 0; i < nums.length; i++){
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[i]);
+        }
+        int ans = (max - min) * k;
+        return ans;
+    }
+}

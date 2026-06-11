bd6d751c1bfd19da409da6bf817eb35b8276872b
Venkateshpaitwar <venkateshpaitwar@gmail.com>
2026-06-11 15:53:04 +0530

    392. Is Subsequence

---
+++ IsSubsequence.java
@@ -0,0 +1,18 @@
+// 392. Is Subsequence
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int j = 0;
+        int count = 0;
+        for(int i = 0; i < t.length(); i++){
+            if(j < s.length() && s.charAt(j) == t.charAt(i)){
+                count++;
+                j++;
+            }
+        }
+        if(count == s.length()){
+            return true;
+        }
+        return false;
+    }
+}

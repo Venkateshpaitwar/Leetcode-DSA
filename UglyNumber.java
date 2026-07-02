b967aee97b1f8e223f5eba8bdfeb7ea8ba58b9a2
Venkateshpaitwar <venkateshpaitwar@gmail.com>
2026-07-02 20:23:23 +0530

    263. Ugly Number

---
+++ UglyNumber.java
@@ -0,0 +1,17 @@
+// 263. Ugly Number
+
+class Solution {
+    public boolean isUgly(int n) {
+        if(n <= 0){
+            return false;
+        }
+
+        int[] factors = {2, 3, 5};
+        for(int factor : factors){
+            while(n % factor == 0){
+                n /= factor;
+            }
+        }
+        return n == 1;
+    }
+}

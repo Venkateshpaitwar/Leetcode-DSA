4c17f52625d2c3b241b0e9457f20f4e41739846d
Venkateshpaitwar <venkateshpaitwar@gmail.com>
2026-08-08 19:17:36 +0530

    216. Combination Sum III

---
+++ CombinationSumIII.java
@@ -0,0 +1,25 @@
+// 216. Combination Sum III
+
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(k, n, 1, new ArrayList<>(), result);
+        return result;
+    }
+    private void backtrack( int k, int target, int start, List<Integer> current, List<List<Integer>> result) {
+        if (current.size() == k) {
+            if (target == 0) {
+                result.add(new ArrayList<>(current));
+            }
+            return;
+        }
+        if (target < 0) {
+            return;
+        }
+        for (int i = start; i <= 9; i++) {
+            current.add(i);
+            backtrack( k, target - i, i + 1, current, result);
+            current.remove(current.size() - 1);
+        }
+    }
+}

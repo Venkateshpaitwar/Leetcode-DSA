605196ea164631b1c58f44eac731aee0a9023186
Venkateshpaitwar <venkateshpaitwar@gmail.com>
2026-08-17 20:04:05 +0530

    Space optimization for 416. Partition Equal Subset Sum

--- partitionSubsetSum.java
+++ partitionSubsetSum.java
@@ -1,5 +1,6 @@
 //416. Partition Equal Subset Sum
 
+/*
 class Solution {
     Boolean[][] memo;
     int[] nums;
@@ -30,5 +31,35 @@ class Solution {
                 dfs(i + 1, remaining);
 
         return memo[i][remaining];
+    }
+}
+ */
+
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for(int num : nums) sum += num;
+        if(sum % 2 != 0) return false;
+
+        int target = sum / 2;
+        boolean[] prev = new boolean[target + 1];
+        if(nums[0] <= target){
+            prev[nums[0]] = true;
+        }
+        prev[0] = true;
+        for(int i = 1; i < nums.length; i++){
+            boolean[] curr = new boolean[target + 1];
+            curr[0] = true;        
+            for(int k = 1; k < target + 1; k++){
+                boolean notTake = prev[k];
+                boolean take = false;
+                if(nums[i] <= k){
+                    take = prev[k - nums[i]];
+                } 
+                curr[k] = take | notTake;
+            }
+            prev = curr;
+        }
+        return prev[target];
     }
 }

07dc7960d1b9f51175d6b0719220bac9994e92ac
Venkateshpaitwar <venkateshpaitwar@gmail.com>
2026-08-18 19:30:36 +0530

    347. Top K Frequent Elements

--- KFrequentElem.java
+++ KFrequentElem.java
@@ -9,9 +9,9 @@ class Solution {
         PriorityQueue<int[]> pq = new PriorityQueue<>(
             (a, b) -> {
                 if (a[1] != b[1]) {
-                    return Integer.compare(b[1], a[1]);
+                    return Integer.compare(a[1], b[1]);
                 }
-                return Integer.compare(b[0], a[0]);
+                return Integer.compare(a[0], b[0]);
                 }
             );
         int[] count = new int[20001];

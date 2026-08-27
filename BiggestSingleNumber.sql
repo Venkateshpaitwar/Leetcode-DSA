262c8fe18429f7bd325634b41ab60f67a0e8d61f
Venkateshpaitwar <venkateshpaitwar@gmail.com>
2026-08-27 20:25:22 +0530

    619. Biggest Single Number

---
+++ BiggestSingleNumber.sql
@@ -0,0 +1,9 @@
+// 619. Biggest Single Number
+
+# Write your MySQL query statement below
+SELECT ifnull(num, 'null') as num
+FROM MyNumbers
+GROUP BY num
+HAVING COUNT(num) = 1
+ORDER BY num DESC
+LIMIT 1
